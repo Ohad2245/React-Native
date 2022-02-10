dbed63c0a98c5621db1a9ad00e1a2c78ca7137fe
Ohad <92857359+Ohad2245@users.noreply.github.com>
2022-02-10 15:05:12 +0200

    Add files via upload

---
+++ StoreApp2/my-app/components/database/Database.js
@@ -0,0 +1,158 @@
+export const COLORS = {
+    white: '#ffffff',
+    black: '#000000',
+    green: '#00AC76',
+    red: '#C04345',
+    blue: '#0043F9',
+    backgrondLight: '#F0F0F3',
+    backgroundMedium: '#B9B9B9',
+    backgroundDark: '#777777',
+}
+
+export const Items = [
+    {
+        id:1,
+        category:'product',
+        productName:'Air Gorden1',
+        productPrice:1900,
+        descreptaion:'Nice Shoose',
+        isOff:true,
+        offPercentage:10,
+        productImage: require('../database/images/products/nike1.jpg'),
+        isAvailable:true,
+        productImageList:[
+            require('../database/images/products/nike1.jpg'),
+            require('../database/images/products/nike2.jpg'),
+            require('../database/images/products/nike3.jpg'), 
+        ]
+},
+{
+    id:2,
+    category:'product',
+    productName:'Air Gorden2',
+    productPrice:1986,
+    descreptaion:'Nice Shoose',
+    isOff:false,
+    offPercentage:10,
+    productImage: require('../database/images/products/nike2.jpg'),
+    isAvailable:true,
+    productImageList:[
+        require('../database/images/products/nike1.jpg'),
+        require('../database/images/products/nike2.jpg'),
+        require('../database/images/products/nike3.jpg'), 
+    ]
+},
+{
+    id:3,
+    category:'product',
+    productName:'Air Gorden3',
+    productPrice:1900,
+    descreptaion:'Nice Shoose',
+    isOff:true,
+    offPercentage:18,
+    offPercentage:10,
+    productImage: require('../database/images/products/nike1.jpg'),
+    isAvailable:true,
+    productImageList:[
+        require('../database/images/products/nike1.jpg'),
+        require('../database/images/products/nike2.jpg'),
+        require('../database/images/products/nike3.jpg'), 
+    ]
+},
+{
+    id:4,
+    category:'accessory',
+    productName:'Air Gorden3',
+    productPrice:1900,
+    descreptaion:'Nice Shoose',
+    isOff:true,
+    offPercentage:10,
+    productImage: require('../database/images/accessories/nike1.jpg'),
+    isAvailable:false,
+    productImageList:[
+        require('../database/images/products/nike1.jpg'),
+        require('../database/images/products/nike2.jpg'),
+        require('../database/images/products/nike3.jpg'), 
+    ]
+},
+{
+    id:5,
+    category:'accessory',
+    productName:'Air Gorden3',
+    productPrice:1900,
+    descreptaion:'Nice Shoose',
+    isOff:false,
+    offPercentage:10,
+    productImage: require('../database/images/accessories/nike1.jpg'),
+    isAvailable:true,
+    productImageList:[
+        require('../database/images/accessories/nike1.jpg'),
+        require('../database/images/accessories/nike2.jpg'),
+        require('../database/images/accessories/nike3.jpg'), 
+    ]
+},
+{
+    id:6,
+    category:'accessory',
+    productName:'Air Gorden3',
+    productPrice:1900,
+    descreptaion:'Nice Shoose',
+    isOff:true,
+    offPercentage:10,
+    productImage: require('../database/images/accessories/nike3.jpg'),
+    isAvailable:true,
+    productImageList:[
+        require('../database/images/accessories/nike1.jpg'),
+        require('../database/images/accessories/nike2.jpg'),
+        require('../database/images/accessories/nike3.jpg'), 
+    ]
+},
+{
+    id:7,
+    category:'accessory',
+    productName:'Air Gorden4',
+    productPrice:1900,
+    descreptaion:'Nice Shoose',
+    isOff:false,
+    offPercentage:10,
+    productImage: require('../database/images/accessories/nike2.jpg'),
+    isAvailable:false,
+    productImageList:[
+        require('../database/images/accessories/nike1.jpg'),
+        require('../database/images/accessories/nike2.jpg'),
+        require('../database/images/accessories/nike3.jpg'), 
+    ]
+},
+{
+    id:8,
+    category:'children',
+    productName:'Air Gorden4',
+    productPrice:1900,
+    descreptaion:'Nice Shoose',
+    isOff:false,
+    offPercentage:10,
+    productImage: require('../database/images/children/nike2.jpg'),
+    isAvailable:false,
+    productImageList:[
+        require('../database/images/children/nike1.jpg'),
+        require('../database/images/children/nike2.jpg'),
+        require('../database/images/children/nike3.jpg'), 
+    ]
+},
+{
+    id:9,
+    category:'children',
+    productName:'Air Gorden4',
+    productPrice:250,
+    descreptaion:'Nice Shoose',
+    isOff:false,
+    offPercentage:10,
+    productImage: require('../database/images/children/nike2.jpg'),
+    isAvailable:false,
+    productImageList:[
+        require('../database/images/children/nike1.jpg'),
+        require('../database/images/children/nike2.jpg'),
+        require('../database/images/children/nike3.jpg'), 
+    ]
+},
+]
