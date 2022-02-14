de4907544c5c2dcf17238a952fadf288e99f1e46
Ohad <92857359+Ohad2245@users.noreply.github.com>
2022-02-14 19:25:43 +0200

    Add files via upload

---
+++ StoreApp2/my-app/database/Database.js
@@ -0,0 +1,166 @@
+import Product from '../models/Product';
+
+export const Items = [
+    new Product(
+        1, // id
+        'product', // category
+        'Air Gorden1', // productName
+        1900, // productPrice
+        'Nice Shoose', // descreptaion
+        true, // isOff
+        10, // offPercentage
+        // productImage
+        require('../assets/images/Football/alpha-menace/alpha-menace-1.jpg'),
+        true, // isAvailable
+        // productImageList
+        [
+            require('../assets/images/Football/alpha-menace/alpha-menace-2.jpg'),
+            require('../assets/images/Football/alpha-menace/alpha-menace-3.jpg'),
+            require('../assets/images/Football/alpha-menace/alpha-menace-4.jpg'),
+        ],
+    ),
+    new Product(
+    2,
+    'product',
+    'Air Gorden2',
+    1986,
+    'Nice Shoose',
+    false,
+    10,
+     require('../assets/images/Football/force-savage/force-savage-1.jpg'),
+    true,
+        [
+            require('../assets/images/Football/force-savage/force-savage-2.jpg'),
+            require('../assets/images/Football/force-savage/force-savage-3.jpg'),
+            require('../assets/images/Football/force-savage/force-savage-4.jpg'),
+        ],
+    ),
+    new Product(
+    3,
+    'product',
+    'Air Gorden3',
+    1900,
+    'Nice Shoose',
+    true,
+    10,
+     require('../assets/images/Football/vapor-edge/vapor-edge-1.jpg'),
+    true,
+    [
+        require('../assets/images/Football/vapor-edge/vapor-edge-1.jpg'),
+        require('../assets/images/Football/vapor-edge/vapor-edge-1.jpg'),
+        require('../assets/images/Football/vapor-edge/vapor-edge-1.jpg'),
+    ],
+    ),
+    new Product(
+    4,
+    'product',
+    'Air Gorden3',
+    1900,
+    'Nice Shoose',
+    true,
+    10,
+    require('../assets/images/Football/vapor-edge-elite/vapor-edge-elite-1.jpg'),
+    false,
+    [
+        require('../assets/images/Football/vapor-edge-elite/vapor-edge-elite-2.jpg'),
+        require('../assets/images/Football/vapor-edge-elite/vapor-edge-elite-3.jpg'),
+        require('../assets/images/Football/vapor-edge-elite/vapor-edge-elite-4.jpg'),
+    ],
+    ),
+    new Product(
+    5,
+    'accessory',
+    'Air Gorden3',
+    1900,
+    'Nice Shoose',
+    false,
+    10,
+    require('../assets/images/accessories/nike1.jpg'),
+    true,
+    [
+        require('../assets/images/accessories/nike1.jpg'),
+        require('../assets/images/accessories/nike2.jpg'),
+        require('../assets/images/accessories/nike3.jpg'), 
+    ],
+    ),
+    new Product(
+    6,
+    'accessory',
+    'Air Gorden3',
+    1900,
+    'Nice Shoose',
+    true,
+    10,
+     require('../assets/images/accessories/nike3.jpg'),
+    true,
+    [
+        require('../assets/images/accessories/nike1.jpg'),
+        require('../assets/images/accessories/nike2.jpg'),
+        require('../assets/images/accessories/nike3.jpg'), 
+    ],
+    ),
+    new Product(
+    7,
+    'accessory',
+    'Air Gorden4',
+    1900,
+    'Nice Shoose',
+    false,
+    10,
+    require('../assets/images/accessories/nike2.jpg'),
+    false,
+    [
+        require('../assets/images/accessories/nike1.jpg'),
+        require('../assets/images/accessories/nike2.jpg'),
+        require('../assets/images/accessories/nike3.jpg'), 
+    ],
+    ),
+    new Product(
+    8,
+    'children',
+    'Air Gorden4',
+    1900,
+    'Nice Shoose',
+    false,
+    10,
+    require('../assets/images/children/nike2.jpg'),
+    false,
+    [
+        require('../assets/images/children/nike1.jpg'),
+        require('../assets/images/children/nike2.jpg'),
+        require('../assets/images/children/nike3.jpg'), 
+    ],
+    ),
+    new Product(
+    9,
+    'children',
+    'Air Gorden4',
+    250,
+    'Nice Shoose',
+    false,
+    10,
+    require('../assets/images/children/nike2.jpg'),
+    false,
+    [
+        require('../assets/images/children/nike1.jpg'),
+        require('../assets/images/children/nike2.jpg'),
+        require('../assets/images/children/nike3.jpg'), 
+    ],
+    ),
+    new Product(
+        10,
+        'children',
+        'Air Gorden7',
+        250,
+        'Nice Shoose',
+        false,
+        10,
+        require('../assets/images/children/nike6.jpg'),
+        false,
+        [
+            require('../assets/images/children/nike1.jpg'),
+            require('../assets/images/children/nike2.jpg'),
+            require('../assets/images/children/nike3.jpg'), 
+        ],
+        ),
+];
